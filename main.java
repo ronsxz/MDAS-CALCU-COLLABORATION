081a9faf39d25ab0e7a942520474fc466c9b2b56
ronsxz <johnaaronfulay@gmail.com>
2026-01-29 14:09:22 +0000

    calculator body

---
+++ main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        double num1 = 0;
+        double num2 = 0;
+        double result = 0;
+
+        System.out.print("Enter first number: ");
+        num1 = scan.nextDouble();
+        System.out.print("Enter second number: ");
+        num2 = scan.nextDouble();
+        System.out.print("Enter operators (+, -, *): ");
+        char operators = scan.next().charAt(0);
+
+        switch(operators){          
+            default:
+        }
+        System.out.println("Result: " + result);
+        scan.close();
+    }
+}
